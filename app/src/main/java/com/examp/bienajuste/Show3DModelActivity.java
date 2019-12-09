8ff7d0fd8e336e65fbb224f188d2619d8f38c89f
Airsleep <Airsleep>
2019-12-09 13:26:26 +0900

    model scaling adapted

--- app/src/main/java/com/examp/bienajuste/Show3DModelActivity.java
+++ app/src/main/java/com/examp/bienajuste/Show3DModelActivity.java
@@ -111,18 +111,24 @@ public class Show3DModelActivity extends AppCompatActivity {
                     anchorNode.setParent(arFragment.getArSceneView().getScene());
                     if(modelOne){
                         TransformableNode modelone = new TransformableNode(arFragment.getTransformationSystem());
+                        modelone.getScaleController().setMinScale(0.00001f);
+                        modelone.getScaleController().setMaxScale(0.00003f);
                         modelone.setParent(anchorNode);
                         modelone.setRenderable(modelOneRenderable);
                         modelone.select();
                     }
                     else if(modelTwo){
                         TransformableNode modeltwo = new TransformableNode(arFragment.getTransformationSystem());
+                        modeltwo.getScaleController().setMinScale(0.00001f);
+                        modeltwo.getScaleController().setMaxScale(0.00003f);
                         modeltwo.setParent(anchorNode);
                         modeltwo.setRenderable(modelTwoRenderable);
                         modeltwo.select();
                     }
                     else if(modelThree) {
                         TransformableNode modelThree = new TransformableNode(arFragment.getTransformationSystem());
+                        modelThree.getScaleController().setMinScale(0.00001f);
+                        modelThree.getScaleController().setMaxScale(0.00003f);
                         modelThree.setParent(anchorNode);
                         modelThree.setRenderable(modelThreeRenderable);
                         modelThree.select();
